3e4b584676c19887b8c5c81364cae8a963c88edc
Bradford Knowlton <brad@ucanbonline.net>
2014-11-27 12:44:40 -0700

    Initial Commit

---
+++ WP-affiliate-system.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * WP Affiliate System
+ *
+ * @package   WP-affiliate-system
+ * @author    Bradford Knowlton
+ * @license   GPL-2.0+
+ * @link      https://github.com/bradmkjr/WP-affiliate-system
+ */
+
+/*
+Plugin Name:       WP Affiliate System
+Plugin URI:        https://github.com/bradmkjr/WP-affiliate-system
+Description:       Top Secret
+Version:           3.0.0
+Author:            Bradford Knowlton
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:       github-updater
+GitHub Plugin URI: https://github.com/bradmkjr/WP-affiliate-system
+GitHub Branch:     master
+Requires WP:       4.0.1
+*/
